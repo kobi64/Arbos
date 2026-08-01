c37b100f4caf6689d592dddec1a14780af8eb828
root <root@vmi3466465.contaboserver.net>
2026-08-01 17:36:29 +0200

    EX-003: add network registry tests

---
+++ tests/test_network_registry.py
@@ -0,0 +1,53 @@
+from exchanges.network_registry import NetworkInfo, NetworkRegistry
+
+
+def test_add_and_get_networks():
+    registry = NetworkRegistry()
+
+    registry.add_network(NetworkInfo("USDT", "TRC20"))
+
+    networks = registry.get_networks("USDT")
+
+    assert len(networks) == 1
+    assert networks[0].network == "TRC20"
+
+
+def test_coin_lookup_is_case_insensitive():
+    registry = NetworkRegistry()
+
+    registry.add_network(NetworkInfo("usdt", "TRC20"))
+
+    networks = registry.get_networks("UsDt")
+
+    assert len(networks) == 1
+
+
+def test_executable_networks_excludes_maintenance():
+    registry = NetworkRegistry()
+
+    registry.add_network(NetworkInfo("USDT", "TRC20"))
+    registry.add_network(
+        NetworkInfo("USDT", "ERC20", maintenance=True)
+    )
+
+    executable = registry.executable_networks("USDT")
+
+    assert len(executable) == 1
+    assert executable[0].network == "TRC20"
+
+
+def test_executable_networks_requires_deposit_and_withdrawal():
+    registry = NetworkRegistry()
+
+    registry.add_network(
+        NetworkInfo("USDT", "TRC20", deposit_enabled=False)
+    )
+    registry.add_network(
+        NetworkInfo("USDT", "ERC20", withdraw_enabled=False)
+    )
+    registry.add_network(NetworkInfo("USDT", "BEP20"))
+
+    executable = registry.executable_networks("USDT")
+
+    assert len(executable) == 1
+    assert executable[0].network == "BEP20"
